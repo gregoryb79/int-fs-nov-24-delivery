c32440edb387b2e5f37955ea96147aedae067385
gregoryb79 <gregoryb79@gmail.com>
2025-05-14 20:59:01 +0300

    404fix

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router";
 import { Main } from "../../../int-fs-nov-24-delivery-1/src/components/Main";
-import { listOrders, timestampFormater, type OrderList } from "../../../int-fs-nov-24-delivery-1/src/models/order";
 
-import styles from "./OrderHistory.module.scss";
 
 export function Page404() {
     
